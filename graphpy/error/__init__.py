55921043099acd6e8cd050d2015f9f94fa809320
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-06-11 05:22:54 -0300

    :hammer: added error package

---
+++ graphpy/error/__init__.py
@@ -0,0 +1,1 @@
+from .graph_incompatible import GraphIncompatibleError
---
+++ graphpy/error/graph_incompatible.py
@@ -0,0 +1,6 @@
+class GraphIncompatibleError(Exception):
+    def __init__(self, msg: str):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
