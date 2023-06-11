8b368bd5c3f1c27006c123b9ed6d70d298acd5a2
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-06-11 16:56:03 -0300

    :heavy_check_mark: updated test of Graph class

--- tests/test_graphpy.py
+++ tests/test_graphpy.py
@@ -1,5 +1,38 @@
-def test_makefile():
-    '''
-    This test function is just to not cause problems in the Makefile
-    '''
-    assert 1 + 1 == 2
+import pytest
+
+from graphpy import Graph
+
+
+@pytest.fixture
+def graph():
+    shape = (6, 6)
+    sparse = False
+    directed = True
+    weighted = False
+    return Graph(shape, sparse=sparse, directed=directed, weighted=weighted)
+
+
+@pytest.fixture
+def graph_with_weight():
+    shape = (10, 10)
+    sparse = False
+    directed = True
+    weighted = True
+    return Graph(shape, sparse=sparse, directed=directed, weighted=weighted)
+
+
+def test_graph_creation(graph):
+    assert graph.shape == (6, 6)
+    assert graph.is_sparse is False
+    assert graph.is_directed is True
+    assert graph.is_weighted is False
+
+
+def test_graph_set_and_get_item(graph):
+    graph[1] = 2
+    assert graph[1][2] == 1
+
+
+def test_graph_set_and_get_item_with_weight(graph_with_weight):
+    graph_with_weight[1] = (2, 0.5)
+    assert graph_with_weight[1][2] == 0.5
